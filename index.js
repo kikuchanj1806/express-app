794543a37b2d99720d1a72c9bfe5b31a7b083464
tunglx <tunglx1@nhanh.vn>
2023-06-23 10:23:23 +0700

    fix 6

--- index.js
+++ index.js
@@ -2,8 +2,8 @@ const express = require("express");
 const app = express();
 const product = require("./api/product");
 
-app.set('view engine', 'ejs');
-app.set('views', path.join(__dirname, 'views'));
+// app.set('view engine', 'ejs');
+// app.set('views', path.join(__dirname, 'views'));
 app.use(cors());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
