e7cdf41945e2327c5f91a46d5667d3c575d4dd42
tunglx <tunglx1@nhanh.vn>
2023-06-23 10:26:03 +0700

    fix 8

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const product = require("./api/product");
 
 // app.set('view engine', 'ejs');
 // app.set('views', path.join(__dirname, 'views'));
-app.use(cors());
+// app.use(cors());
 // app.use(bodyParser.json());
 // app.use(bodyParser.urlencoded({ extended: true }));
 
